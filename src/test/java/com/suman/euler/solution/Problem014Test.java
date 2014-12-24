ac8b63628d037e9460a7df96c3911620722bcc40
palsuman <mail.suman.sss@gmail.com>
2014-12-24 12:37:49 +0530

    Commit problem 14

---
+++ src/test/java/com/suman/euler/solution/Problem014Test.java
@@ -0,0 +1,41 @@
+package com.suman.euler.solution;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.suman.euler.problem.EulerTest;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The class <code>Problem007Test</code> contains tests for the class {@link <code>Problem007</code>}
+ * .
+ *
+ * @author icteas
+ * @version $Revision$
+ * @pattern JUnit Test Case
+ * @generatedBy CodePro at 19/12/14 2:40 PM
+ */
+public class Problem014Test extends EulerTest {
+
+	/** The expected. */
+	private final long expected = 70600674L;
+
+	/**
+	 * Construct new test instance.
+	 */
+	public Problem014Test() {
+		super(new Problem014());
+	}
+
+	/**
+	 * Run the Object executeSoultion() method test.
+	 */
+	@Test(timeout = TIME_OUT)
+	public void testExecuteSoultion() {
+		long actual = (long) eulerProblem.executeSoultion();
+		this.result = actual;
+		assertEquals(expected, actual);
+	}
+
+}
