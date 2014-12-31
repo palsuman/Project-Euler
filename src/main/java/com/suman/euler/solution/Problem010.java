bbb626b90b0d2196ac773e910aba2902000f6193
palsuman <mail.suman.sss@gmail.com>
2014-12-31 18:03:59 +0530

    Remove exception handling

--- src/main/java/com/suman/euler/solution/Problem010.java
+++ src/main/java/com/suman/euler/solution/Problem010.java
@@ -21,19 +21,13 @@ public class Problem010 implements EulerProblem {
 		long sumOfPrime = 2;
 
 		for (long index = 3; index < 2000000; index += Function.MAX_DIFFERENCE + 1) {
-			try {
-				long range = index + Function.MAX_DIFFERENCE;
-				if (range >= 2000000) {
-					range = 2000000;
-				}
-				List<Long> primeSequence = Function.getPrimeSequence(index, range);
-				for (Long prime : primeSequence) {
-					sumOfPrime += prime;
-				}
-				
-
-			} catch (Exception e) {
-				e.printStackTrace();
+			long range = index + Function.MAX_DIFFERENCE;
+			if (range >= 2000000) {
+				range = 2000000;
+			}
+			List<Long> primeSequence = Function.getPrimeSequence(index, range);
+			for (Long prime : primeSequence) {
+				sumOfPrime += prime;
 			}
 		}
 		return sumOfPrime;
